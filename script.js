c24373d9773d5971bb81953d951dc3376ca38eb6
gibugatti <g9092873@gmail.com>
2025-03-05 20:31:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+ document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".animate-slide");
+    const options = {
+        threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
