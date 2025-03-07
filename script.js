49ab6d65a90607d837ea33963ab5066a2fb4397b
gibugatti <g9092873@gmail.com>
2025-03-06 21:27:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.querySelector("form").addEventListener("submit", function(event) {
   event.preventDefault();
   alert("Mensagem enviada com sucesso!");
 });
+function toggleSection(sectionId) {
+  // Esconde todas as seções
+  document.querySelectorAll("section:not(#home)").forEach(section => {
+    section.classList.add("hidden");
+  });
+
+  // Exibe a seção clicada
+  document.getElementById(sectionId).classList.remove("hidden");
+}
