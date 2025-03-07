efad6a2222d7744100ec0fae7f13f5e96527aea2
gibugatti <g9092873@gmail.com>
2025-03-07 10:19:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,34 @@ function changePage(pageIndex) {
 
   currentPage = pageIndex;
 }
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script>
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "move": {
+        "speed": 2
+      }
+    }
+  });
+</script>
